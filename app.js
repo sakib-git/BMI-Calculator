9ce499badd8e1139418936bfe4a6741e4cdaa426
sakib-git <sakibulhasan32002@gmail.com>
2025-07-28 19:52:29 +0600

    minor fixes

--- app.js
+++ app.js
@@ -13,16 +13,16 @@ const h = parseFloat(height.value)
 if(w >= 100 && h >= 6){
   result.innerText = ' Obese'
 }
-else if(w >= 80 && w <= 99 && h >= 5.7 && h <= 6){
+else if(w >= 80 && w <= 99 &&  h <= 6){
   result.innerText = 'Overweight'
 }
-else if(w >= 65 && w <= 79 && h >= 5.6 && h <= 6){
+else if(w >= 65 && w <= 79 &&  h <= 6){
   result.innerText = 'Normal weight'
 }
-else if( w >= 45 && w <= 64 && h >= 5 && h <= 5.6){
+else if( w >= 45 && w <= 64 &&  h <= 5.6){
   result.innerText = 'Underweight'
 }
-else if(w >= 20 && w <= 44 && h >= 5 && h <= 5.5){
+else if(w >= 20 && w <= 44 &&  h <= 6){
   result.innerText = 'Beshi beshi khete hobe'
 }
 else{
