02af3b2a13cc94bb59d05bc3936be15208e9fa16
sakib-git <sakibulhasan32002@gmail.com>
2025-07-28 17:01:36 +0600

    html and css and js

---
+++ app.js
@@ -0,0 +1,33 @@
+const btn = document.getElementById('btn')
+const result = document.getElementById('result')
+const weight = document.getElementById('weight')
+const height= document.getElementById('height')
+
+
+
+btn.addEventListener('click', () =>{
+  const w = parseFloat(weight.value)
+const h = parseFloat(height.value)
+
+
+if(w > 110 && h > 6.5){
+  result.innerText = ' Obese'
+}
+else if(w >= 80 && w <= 99 && h >= 5.7 && h <= 6){
+  result.innerText = 'Overweight'
+}
+else if(w >= 65 && w <= 79 && h >= 5.6 && h <= 6){
+  result.innerText = 'Normal weight'
+}
+else if( w >= 40 && w <= 64 && h >= 5.3 && h <= 5.6){
+  result.innerText = 'Underweight'
+}
+else if(w >= 20 && w <= 39 && h >= 5 && h <= 5.5){
+  result.innerText = 'Beshi beshi khete hobe'
+}
+else{
+  result.innerText = 'no result'
+}
+
+
+})
