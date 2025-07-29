adcd2aaf5869978c39d2398d02b113bdc213766c
sakib-git <sakibulhasan32002@gmail.com>
2025-07-29 06:31:11 +0600

    minor fixes

--- app.js
+++ app.js
@@ -9,25 +9,32 @@ btn.addEventListener('click', () =>{
   const w = parseFloat(weight.value)
 const h = parseFloat(height.value)
 
+const heightM = h * 0.3048 // feet ke meter kora hoiche
+ 
 
-if(w >= 100 && h >= 4){
-  result.innerText = ' Obese'
+const BMI = w / (heightM * heightM)
+
+
+  
+
+ if(BMI >= 30){
+  result.innerText = `BMI: ${BMI.toFixed(2)} - Obese ` 
 }
-else if(w >= 80 && w <= 99 &&  h <= 10){
-  result.innerText = 'Overweight'
+else if(BMI >= 25.5 && BMI <= 29.5){
+  result.innerText = `BMI: ${BMI.toFixed(2)} - Overweight`
 }
-else if(w >= 65 && w <= 79 &&  h <= 10){
-  result.innerText = 'Normal weight'
+else if(BMI >= 18.5 && BMI <=24.5){
+  result.innerText = `BMI: ${BMI.toFixed(2)} Normal weight`
 }
-else if( w >= 45 && w <= 64 &&  h <= 10){
-  result.innerText = 'Underweight'
+else if(BMI <= 17.5){
+  result.innerText = `BMI: ${BMI.toFixed(2)} - Underweight`
 }
-else if(w >= 20 && w <= 44 &&  h <= 10){
-  result.innerText = 'Beshi beshi khete hobe'
-}
+
 else{
   result.innerText = 'no result'
 }
+
+
 
 
 })
